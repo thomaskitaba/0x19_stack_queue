0fa5d2f6b89eee6a7b769019bbf4d326d1a425c8
thomaskitaba <thomaskitaba@gmail.com>
2023-05-25 15:29:54 +0300

    sq_pstr

--- 11-pchar.c
+++ 11-pchar.c
@@ -7,5 +7,29 @@
 */
 void sq_pchar(stack_t **stack, unsigned int counter)
 {
-    return;
+    stack_t *current;
+    int value;
+
+    current = *stack;
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L<%d>: can't pchar, stack empty\n", counter);
+        fclose(info.file);
+        free(info.content);
+        free_stack(*stack);
+    }
+    value = current->n;
+    if (value < 0 && value > 127)
+    {
+        fprintf(stderr, "L<%d>: can't pchar, value out of range\n", counter);
+        fclose(info.file);
+        free(info.content);
+        free_stack(*stack);
+    }
+    {
+        printf("asci for [%d] = \"%c\"\n", value, value);
+    }
+
+
+
 }
