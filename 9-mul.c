0980fe976740eff7fb4161861d13756dd4e559c3
thomaskitaba <thomaskitaba@gmail.com>
2023-05-25 13:25:57 +0300

    sq_mul

--- 9-mul.c
+++ 9-mul.c
@@ -7,5 +7,26 @@
 */
 void sq_mul(stack_t **stack, unsigned int counter)
 {
-    return;
+    stack_t *current, *Next;
+	current = Next = *stack;
+	int num1, num2, result;
+
+	num1 = num2 = result = 0;
+	if (current->next == NULL)
+	{
+		fprintf(stderr, "L<%d>: can't div, stack too short", counter);
+		fclose(info.file);
+		free(info.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	num1 = current->n;
+	num2 = current->next->n;
+    result = num2 * num1;
+    current->next->n = result;
+    printf("mul = [%d] * [%d] = %d\n", num2, num1, result);
+    printf("Inserted [%d] on second node of value [%d] and poped [%d] from Top\n" , result, num2, num1);
+    sq_pop(stack, counter);
+
 }
