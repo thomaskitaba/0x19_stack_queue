7a0930ee1ae0aa47c45147271291cbab4be0c2d5
thomaskitaba <thomaskitaba@gmail.com>
2023-05-25 20:18:46 +0300

    update

--- 15-rotr.c
+++ 15-rotr.c
@@ -5,15 +5,7 @@
 * @counter: line number of the code
 * Return: nothing
 */
-void sq_rotr(stack_t **stack, unsigned int counter)
-{
-#include "monty.h"
-/**
-* sq_rotl - rotates the stack to the top
-* @stack: head of the stack
-* @counter: line number of the code
-* Return: nothing
-*/
+
 void sq_rotl(stack_t **stack, unsigned int counter)
 {
     stack_t *current, *Next, *temp;
@@ -36,5 +28,4 @@ void sq_rotl(stack_t **stack, unsigned int counter)
         Next =  Next->next;
     }
     *stack = current;
-}
 }
